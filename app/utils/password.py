6b252797a6eb77dd7fca6cd974982e4f3a063768
nikh1508 <kr.nikhil1508@gmail.com>
2021-03-14 21:42:23 +0530

    hashing implemented | added migrations and favicon

--- app/utils/password.py
+++ app/utils/password.py
@@ -1,13 +1,12 @@
+from extensions import bcrypt
 class Password:
     @staticmethod
     def gen_hash(password: str) -> str:
         if not password:
             raise Exception("Password is NULL")
-        # TODO Implement hashing
-        _hashed = password
+        _hashed = bcrypt.generate_password_hash(password).decode('utf-8')
         return _hashed
 
     def verify_password(password, hashed) -> bool:
-        # TODO
-        is_correct = password == hashed
+        is_correct = bcrypt.check_password_hash(hashed, password)
         return is_correct
